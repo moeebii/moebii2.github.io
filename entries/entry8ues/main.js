18d8c473c73ef7aa8d26465e0a5e6b77e0f12bce
moeebii <ebiim621@newschool.edu>
2023-11-20 09:09:02 -0500

    refresh

--- entries/entry8ues/main.js
+++ entries/entry8ues/main.js
@@ -1,2 +1,16 @@
 let navElement = document.querySelector(".nav");
 navElement.classList.add("special");
+var string = "let's explore the neighborhood! ";
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("text").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',70); /* change 70 for speed */
+
+}
+frameLooper();
