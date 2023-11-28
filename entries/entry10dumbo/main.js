7d2c9f0928165d21c127cc62fbef41b6c9a5a5e4
moeebii <ebiim621@newschool.edu>
2023-11-28 07:54:06 -0500

    dumbo

---
+++ entries/entry10dumbo/main.js
@@ -0,0 +1,7 @@
+document.getElementById('popup').style.display = 'block';
+
+document.getElementById('closePopup').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'none';
+});
+
+
