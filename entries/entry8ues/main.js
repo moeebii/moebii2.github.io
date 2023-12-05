3dbf20e54cb2ae5a3f2fe46a4b213de30c8fc47c
moeebii <ebiim621@newschool.edu>
2023-12-05 09:13:19 -0500

    colors

--- entries/entry8ues/main.js
+++ entries/entry8ues/main.js
@@ -14,3 +14,16 @@ function frameLooper () {
 
 }
 frameLooper();
+
+let changeColorElement = document.getElementById("changeColor");
+
+// Define an array of 5 colors
+let colors = ["#FEC0CB", "#cdb4db", "#bdee0fe", "#ede6b2", "#bce9e0"];
+
+function changeBackgroundColor() {
+    let randIndex = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randIndex];
+}
+
+changeColorElement.addEventListener("click", changeBackgroundColor);
+
